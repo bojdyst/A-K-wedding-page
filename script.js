89eae4cdf116ce43d05bf488a0f9fd07cf944f7d
Tomasz Bojdys <tbojdys@student.agh.edu.pl>
2023-02-10 23:35:27 +0100

    Adds css for mobile-friendly content display; decreses resolution of images

---
+++ script.js
@@ -0,0 +1,40 @@
+function closeHeader() {
+  document.getElementById("mainHeader").style.display = "none";
+}
+
+function openLink(evt, animName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("disappear");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("w3-amber", "");
+  }
+  document.getElementById(animName).style.display = "block";
+  evt.currentTarget.className += " w3-amber";
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
